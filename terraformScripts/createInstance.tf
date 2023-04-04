76690630deaf1b368efc5f139b247db9583f5d9e
Jashuwa1993 <129572385+Jashuwa1993@users.noreply.github.com>
2023-04-04 12:44:20 +0530

    Create createInstance.tf

---
+++ terraformScripts/createInstance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+} 
+
+resource "aws_instance" "AWSInstance" {
+    ami = "ami-0fdea1353c525c182"
+    instance_type = "t2.micro"
+    key_name = "jashuwavepanjerirajendran"
+    security_groups = ["launch-wizard-2"]
+    tags = {
+        Name = "dbServers"
+    }
+}
